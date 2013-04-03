d9bb4b800353a3f0add0f01f6ec536d1df89865b
Sébastien Beau <sebastien.beau@akretion.com>
2013-04-03 17:13:57 +0200

    [REF] refactor extract relation between task and file-buffer

---
+++ file_autotask_rel/__openerp__.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+###############################################################################
+#
+#   file_repository for OpenERP
+#   Authors: Emmanuel Samyn <emmanuel.samyn@akretion.com>
+#            Sebastien Beau <sebastien.beau@akretion.com>
+#            Benoît Guillot <benoit.guillot@akretion.com>
+#   Copyright 2013 Akretion
+#
+#   This program is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU Affero General Public License as
+#   published by the Free Software Foundation, either version 3 of the
+#   License, or (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU Affero General Public License for more details.
+#
+#   You should have received a copy of the GNU Affero General Public License
+#   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+###############################################################################
+
+{
+    'name': 'file_repository',
+    'version': '1.0',
+    'category': 'Generic Modules/Others',
+    'license': 'AGPL-3',
+    'description': """
+        Definition : an abstract module that allow to link a file buffer to an
+        automatic task.
+    """,
+    'author': 'Akretion',
+    'website': 'http://www.akretion.com/',
+    'depends': [
+        'abstract_automatic_task',
+        'file_buffer',
+    ],
+    'demo': [],
+    'data': [
+    ],
+    'installable': True,
+    'active': False,
+}
---
+++ file_autotask_rel/file_buffer.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+###############################################################################
+#
+#   file_repository for OpenERP
+#   Authors: Sebastien Beau <sebastien.beau@akretion.com>
+#   Copyright 2013 Akretion
+#
+#   This program is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU Affero General Public License as
+#   published by the Free Software Foundation, either version 3 of the
+#   License, or (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU Affero General Public License for more details.
+#
+#   You should have received a copy of the GNU Affero General Public License
+#   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+###############################################################################
+
+from openerp.osv.orm import Model
+from openerp.osv import fields
+
+available_tasks = []
+
+#TODO improve
+def add_task(name):
+    if not name in available_tasks:
+        available_tasks.append(name)
+
+
+class file_buffer(Model):
+    _inherit = "file.buffer"
+
+    def _get_tasks(self, cr, uid, context=None):
+        model_obj = self.pool.get('ir.model')
+        ids = model_obj.search(cr, uid,
+                               [('model', 'in', available_tasks)],
+                               context=context)
+        res = model_obj.read(cr, uid, ids, ['model', 'name'], context=context)
+        return [(r['model'], r['name']) for r in res]
+
+    _columns = {
+        'task_id': fields.reference('Task', selection=_get_tasks, size=128),
+    }
+
