c996be4ca384f5f8eb872af1382fef976d2074be
davidbeal <david.beal@akretion.com>
2013-10-28 13:03:37 +0100

    [IMP] combine file.document with repository for export from 6.1 branch on file_repository (author: Benoit Guillot)

--- file_repository/file_document.py
+++ file_repository/file_document.py
@@ -3,7 +3,6 @@
 #
 #   file_repository for OpenERP
 #   Authors: Sebastien Beau <sebastien.beau@akretion.com>
-#            David BEAL <david.beal@akretion.com>
 #            Benoît Guillot <benoit.guillot@akretion.com>
 #   Copyright 2013 Akretion
 #
@@ -24,6 +23,8 @@
 
 from openerp.osv import fields, orm
 from openerp.addons.file_autotask_rel.file_document import add_task
+from tempfile import TemporaryFile
+import base64
 
 add_task('repository.task')
 
@@ -32,5 +33,27 @@ class file_document(orm.Model):
     _inherit = "file.document"
 
     _columns = {
-        'repository_id': fields.many2one('file.repository', 'File Repository'),
+        'repository_id': fields.many2one(
+            'file.repository',
+            'File Repository'),
     }
+
+    def export_file_document(self, cr, uid, connection, filedocument,
+                             context=None):
+        outfile = TemporaryFile('w+b')
+        decoded_datas = base64.decodestring(filedocument.datas)
+        outfile.write(decoded_datas)
+        outfile.seek(0)
+        connection.send(filedocument.task_id.home_folder,
+                        filedocument.name, outfile)
+        return outfile
+
+    def _run(self, cr, uid, filedocument, context=None):
+        super(file_document, self)._run(cr, uid, filedocument, context=context)
+        repo_obj = self.pool['file.repository']
+        if filedocument.direction == 'output':
+            connection = repo_obj.repository_connection(
+                cr, uid, filedocument.repository_id.id, context=context)
+            self.export_file_document(cr, uid, connection, filedocument,
+                                      context=context)
+            filedocument.done()
