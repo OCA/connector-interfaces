4fa0e6a1019eba985eb6a7fc72740035f529a22e
Nicolas Bessi <nicolas.bessi@camptocamp.com>
2014-09-15 11:14:52 +0200

    Add import configuration in odbc backend

--- connector_odbc/backend_model.py
+++ connector_odbc/backend_model.py
@@ -35,6 +35,101 @@ install_in_connector()
 MODEL_REGISTRY = []
 
 
+class import_configurator(orm.TransientModel):
+    _name = 'connector.odbc.import.configurator'
+
+    _rec_name = 'model_id'
+
+    _columns = {
+        'model_id': fields.many2one(
+            'ir.model',
+            'What to import'
+        ),
+        'backend_id': fields.many2one(
+            'connector.odbc.data.server.backend',
+            'Backend to configure',
+            required=True
+        ),
+        'priority': fields.integer(
+            'Priority',
+            help='Smaller value = greater priority'
+        ),
+    }
+
+    def _get_default_backend(self, cr, uid, context=None):
+        if context is None:
+            context = self.pool['res.users'].context_get(cr, uid)
+        active_id = context.get('active_id')
+        if not active_id:
+            active_ids = context.get('active_ids', [None])
+            active_id = active_ids[0]
+        assert active_id, "No active_id in context"
+        return active_id
+
+    _defaults = {'backend_id': _get_default_backend}
+
+    def _get_valid_importer(self, cr, uid, backend_id, context=None):
+        """Return the name of valid bining model
+
+        That are bound to a ``ODBCSynchronizer`` subclass
+
+        """
+        backend_model = self.pool['connector.odbc.data.server.backend']
+        model_model = self.pool['ir.model']
+        backend = backend_model.browse(cr, uid, backend_id, context=context)
+        avail_models = set(x._model_name for x in backend._class_entries
+                           if issubclass(x, ODBCSynchronizer))
+        model_ids = self.search(cr, uid, ['model', 'in', avail_models])
+        if not model_ids:
+            raise NotImplementedError(
+                'No class overriding ODBCSynchronizer are available'
+            )
+            return [x.id for x in
+                    model_model.browse(cr, uid, model_ids, context=context)]
+
+    def create_register(self, cr, uid, ids, context=None):
+        register_model = self.pool["connector.odbc.data.import.register"]
+        if isinstance(ids, (int, long)):
+            ids = [ids]
+        assert len(ids) == 1
+        for current in self.browse(cr, uid, ids, context=context):
+            data = {
+                'backend_id': current.backend_id.id,
+                'model_id': current.model_id.id,
+                'sequence': current.priority,
+            }
+            register_model.create(cr, uid, data, context=context)
+        return True
+
+
+class odcb_register(orm.Model):
+    """Configurable connector model register"""
+
+    _name = "connector.odbc.data.import.register"
+
+    _order = 'sequence'
+    _rec_name = 'model_id'
+
+    _columns = {
+        'model_id': fields.many2one(
+            'ir.model',
+            'What to Import',
+            readonly=True,
+            required=True
+        ),
+
+        'sequence': fields.integer(
+            'Priority',
+            required=True
+        ),
+        'backend_id': fields.many2one(
+            'connector.odbc.data.server.backend',
+            'Related Backend',
+            required=True
+        )
+    }
+
+
 class odbc_backend(orm.Model):
     """Base ODBC Data sync backend with odbc supported server"""
 
@@ -74,6 +169,10 @@ class odbc_backend(orm.Model):
             string='Version',
             required=True
         ),
+        'import_register_ids': fields.one2many(
+            'connector.odbc.data.import.register',
+            'backend_id',
+            'Model to import')
     }
 
     def _import(self, cursor, uid, ids, models, mode,
