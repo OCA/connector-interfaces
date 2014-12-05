5623ae8913f7c9ee9ff3a6db5f5d36628051ec3b
Nicolas Bessi <nicolas.bessi@camptocamp.com>
2014-12-05 19:16:36 +0100

    Add connectore_salesforce skeleton and auth tools

---
+++ connector_salesforce/__openerp__.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Author: Nicolas Bessi
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+{'name': 'Salesforce connector',
+ 'version': '0.1',
+ 'author': 'Camptocamp',
+ 'maintainer': 'Camptocamp',
+ 'category': 'Connector',
+ 'complexity': "normal",
+ 'depends': ['connector', 'sale', 'product'],
+ 'summary': """Provides core import export interfaces with Salesforce.""",
+ 'description': """
+Provides core import export interfaces with Salesforce.
+Manage number of rest call limitation.
+
+By default following action are supported:
+
+- Import of Account and contact into OpenERP/Odoo Salesforce is the master
+- Import of Product and PriceBook entries into OpenERP/Odoo
+  Salesforce is the master
+- Import of chosen Opportunity into OpenERP/Odoo Salesforce is the master
+
+Export to Salesforce are supported and implemented but
+there is no default export provided at current time.
+
+""",
+ 'data': ['view/backend_model_view.xml'],
+ 'test': [],
+ 'installable': True,
+ 'auto_install': False,
+ 'license': 'AGPL-3',
+ 'application': False,
+ }
---
+++ connector_salesforce/utils/oauth2_utils.py
@@ -0,0 +1,78 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Author: Nicolas Bessi
+#    Copyright 2014 Camptocamp SA
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+import requests
+from urllib import quote, urlencode
+from urlparse import parse_qs, urljoin
+import simplejson as json
+
+
+class SalesForceOauth2MAnager(object):
+
+    def __init__(self, backend_record):
+        """
+        """
+        self.backend = backend_record
+        self.base_login_url = 'https://login.salesforce.com/'
+        self.authorization_url = "services/oauth2/authorize"
+        self.token_url = "services/oauth2/token"
+        self.redirect_uri = urljoin(self.backend.callback_url,
+                                    "salesforce/oauth")
+        if self.backend.sandbox:
+            self.base_login_url = "https://test.salesforce.com/"
+
+    def authorize_url(self, scope='', **kwargs):
+        """
+        Returns the callback url to redirect the user after authorization
+        """
+
+        oauth_params = {
+            'redirect_uri': self.redirect_uri,
+            'client_id': self.backend.consumer_key,
+            'scope': scope
+        }
+        oauth_params.update(kwargs)
+        return "%s%s?%s" % (
+            self.base_login_url,
+            quote(self.authorization_url),
+            urlencode(oauth_params)
+        )
+
+    def get_token(self, code=None, **kwargs):
+        """
+        Requests an access token
+        """
+        url = "%s%s" % (self.base_login_url, quote(self.token_url))
+        data = {'redirect_uri': self.redirect_uri,
+                'client_id': self.backend.consumer_key,
+                'client_secret': self.backend.consumer_secret}
+        if code:
+                data['code'] = code
+        data.update(kwargs)
+        response = requests.post(url, data=data)
+
+        if isinstance(response.content, basestring):
+            try:
+                content = json.loads(response.content)
+            except ValueError:
+                content = parse_qs(response.content)
+        else:
+            content = response.content
+        return content
