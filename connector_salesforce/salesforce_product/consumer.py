b7f06b7b16ced45954e5f894ceae4c9c1f91f869
Nicolas Bessi <nicolas.bessi@camptocamp.com>
2015-01-05 16:12:24 +0100

    Add product exporter

---
+++ connector_salesforce/salesforce_product/consumer.py
@@ -0,0 +1,85 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Author: Nicolas Bessi
+#    Copyright 2015 Camptocamp SA
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+from openerp.addons.connector.event import (on_record_write,
+                                            on_record_create,
+                                            on_record_unlink)
+from ..consumer import delay_export, delay_deactivate
+
+
+@on_record_create(model_names='connector.salesforce.product')
+@on_record_write(model_names='connector.salesforce.product')
+def export_sf_product(session, model_name, record_id, vals=None):
+    record = session.browse(model_name, record_id)
+    if record.backend_id.sf_product_master == 'erp':
+        delay_export(session, model_name, record_id)
+
+
+@on_record_unlink(model_names='connector.salesforce.product')
+def deactivate_product(session, model_name, record_id):
+    record = session.browse(model_name, record_id)
+    if record.backend_id.sf_product_master == 'erp':
+        delay_deactivate(session, model_name, record_id)
+
+@on_record_create(model_names='product.product')
+def create_product_binding(session, model_name, record_id, vals=None):
+    record = session.browse(model_name, record_id)
+    sf_product_model = 'connector.salesforce.product'
+    backend_model = 'connector.salesforce.backend'
+    backend_ids = session.search(backend_model, [])
+    if not record.sale_ok or not record.active:
+        return
+    for backend in session.browse(backend_model, backend_ids):
+        if backend.sf_product_master == 'erp':
+            session.create(sf_product_model,
+                           {'backend_id': backend.id,
+                            'openerp_id': record_id})
+
+@on_record_write(model_names='product.product')
+def export_product(session, model_name, record_id, vals=None):
+    sf_product_model = 'connector.salesforce.product'
+    backend_model = 'connector.salesforce.backend'
+    backend_ids = session.search(backend_model, [])
+    record = session.browse(model_name, record_id)
+    for backend in session.browse(backend_model, backend_ids):
+        if backend.sf_product_master == 'erp':
+            sf_prod_id = session.search(
+                sf_product_model,
+                [
+                    ('backend_id', '=', backend.id),
+                    ('openerp_id', '=', record_id)
+                ]
+            )
+            if sf_prod_id:
+                assert len(sf_prod_id) == 1
+                export_sf_product(
+                    session,
+                    sf_product_model,
+                    sf_prod_id[0],
+                    vals=vals
+                )
+            else:
+                if vals.get('sale_ok') and record.active:
+                    create_product_binding(
+                        session,
+                        model_name,
+                        record_id,
+                        vals=vals
+                    )
---
+++ connector_salesforce/salesforce_product/exporter.py
@@ -0,0 +1,75 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Author: Nicolas Bessi
+#    Copyright 2015 Camptocamp SA
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+import logging
+from openerp.addons.connector.unit.mapper import ExportMapper
+from openerp.addons.connector.unit.mapper import mapping
+from ..backend import salesforce_backend
+from ..unit.exporter_synchronizer import (SalesforceDelayedBatchSynchronizer,
+                                          SalesforceDirectBatchSynchronizer,
+                                          SalesforceExportSynchronizer)
+
+_logger = logging.getLogger('salesforce_connector_product_export_mapping')
+
+TYPE_MAP_REGISTER = {'Service': 'service'}
+
+
+@salesforce_backend
+class SalesforceProductExporter(SalesforceExportSynchronizer):
+    _model_name = 'connector.salesforce.product'
+
+    def _to_deactivate(self):
+        assert self.binding_record
+        if not self.binding_record.active or not self.binding_record.sale_ok:
+                return True
+        return False
+
+
+@salesforce_backend
+class SalesforceDirectBatchProductExporter(SalesforceDirectBatchSynchronizer):
+    _model_name = 'connector.salesforce.product'
+
+
+@salesforce_backend
+class SalesforceDelayedBatchProductExporter(
+        SalesforceDelayedBatchSynchronizer):
+    _model_name = 'connector.salesforce.product'
+
+
+@salesforce_backend
+class SalesforceProductMapper(ExportMapper):
+    _model_name = 'connector.salesforce.product'
+
+    direct = [
+        ('active', 'IsActive',),
+        ('code', 'ProductCode'),
+        ('description', 'Description'),
+        ('name', 'Name'),
+    ]
+
+    @mapping
+    def product_type(self, record):
+        backend = record.backend_id
+        mapping = {rec.product_type: rec.sf_family
+                   for rec in backend.sf_product_type_mapping_ids}
+        family = mapping.get(record.type)
+        if not family:
+            return {}
+        return {'Family': family}
