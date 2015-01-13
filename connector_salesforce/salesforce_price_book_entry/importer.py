ec39616b83185fbab4fd3d0d3e8e1831fc8e961e
Nicolas Bessi <nicolas.bessi@camptocamp.com>
2015-01-13 13:36:10 +0100

    Refactor price book importer to use general mapper

--- connector_salesforce/salesforce_price_book_entry/importer.py
+++ connector_salesforce/salesforce_price_book_entry/importer.py
@@ -22,12 +22,12 @@ import logging
 from openerp.addons.connector.exception import MappingError
 from openerp.addons.connector.unit.mapper import mapping, only_create
 from ..backend import salesforce_backend
-from openerp.addons.connector.unit.mapper import ImportMapper
 from ..unit.importer_synchronizer import (SalesforceDelayedBatchSynchronizer,
                                           SalesforceDirectBatchSynchronizer,
                                           SalesforceImportSynchronizer,
                                           import_record)
 from ..unit.rest_api_adapter import SalesforceRestAdapter
+from ..unit.mapper import PriceMapper
 _logger = logging.getLogger('salesforce_connector_entry_import')
 
 
@@ -85,7 +85,7 @@ class SalesforcePriceBookEntryAdapter(SalesforceRestAdapter):
 
 
 @salesforce_backend
-class SalesforcePriceBookEntryMapper(ImportMapper):
+class SalesforcePriceBookEntryMapper(PriceMapper):
     _model_name = 'connector.salesforce.pricebook.entry'
 
     direct = [
@@ -96,30 +96,6 @@ class SalesforcePriceBookEntryMapper(ImportMapper):
     @mapping
     def backend_id(self, record):
         return {'backend_id': self.backend_record.id}
-
-    def get_currency_id(self, record):
-        currency_iso_code = record.get('CurrencyIsoCode')
-        if not currency_iso_code:
-            raise MappingError(
-                'No currency Given for price book entry: %s' % record
-            )
-        currency_id = self.session.search(
-            'res.currency',
-            [('name', '=ilike', currency_iso_code)]
-        )
-        if not currency_id:
-            raise MappingError(
-                'No %s currency available. '
-                'Please create one manually' % currency_iso_code
-            )
-        if len(currency_id) > 1:
-            raise ValueError(
-                'Many Currencies found for %s. '
-                'Please ensure your multicompany rules are corrects '
-                'or check that the job is not runned by '
-                'the admin user' % currency_iso_code
-            )
-        return currency_id[0]
 
     @mapping
     def price_version_id(self, record):
