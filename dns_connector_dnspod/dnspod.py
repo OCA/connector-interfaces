78533b7c40a294b14d2b70f964e3b37ba49ea1c9
Liu Lixia <liulixia19901988@163.com>
2015-02-28 12:12:51 +0800

    relsove the pylint problem

--- dns_connector_dnspod/dnspod.py
+++ dns_connector_dnspod/dnspod.py
@@ -20,10 +20,10 @@
 #
 ##############################################################################
 from openerp import models, fields, api
-from connector-interfaces.dns_connector_dnspod.backend import dnspod
-from connector-interfaces.dns_connector_dnspod.unit.export_synchronizer \
+from connector_interfaces.dns_connector_dnspod.backend import dnspod
+from connector_interfaces.dns_connector_dnspod.unit.export_synchronizer \
     import DNSExporter
-from connector-interfaces.dns_connector_dnspod.unit.backend_adapter \
+from connector_interfaces.dns_connector_dnspod.unit.backend_adapter \
     import DNSPodAdapter
 from openerp.addons.connector.unit.mapper import (
     mapping, ExportMapper)
