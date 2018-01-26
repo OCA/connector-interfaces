ca4b2693ba0742fddde63e2e4e2c016ad4f1f364
Simone Orsi <simahawk@gmail.com>
2018-01-26 16:51:05 +0100

    [11.0] connector_importer

--- connector_importer/models/backend.py
+++ connector_importer/models/backend.py
@@ -1,10 +1,7 @@
-# -*- coding: utf-8 -*-
 # Author: Simone Orsi
-# Copyright 2017 Camptocamp SA
+# Copyright 2018 Camptocamp SA
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from contextlib import contextmanager
-from odoo.addons.connector.connector import ConnectorEnvironment
 from odoo import models, fields, api, exceptions, _
 import logging
 
@@ -15,17 +12,13 @@ BACKEND_VERSIONS = [
 ]
 
 
-class ImportBackend(models.Model):
+class ImporterBackend(models.Model):
     _name = 'import.backend'
-    _description = 'Import Backend'
-    _inherit = 'connector.backend'
-    _backend_type = 'import_backend'
-
-    @contextmanager
-    @api.multi
-    def get_environment(self, model_name):
-        self.ensure_one()
-        yield ConnectorEnvironment(self, model_name)
+    _description = 'Importer Backend'
+    _inherit = [
+        'connector.backend',
+        'cron.mixin',
+    ]
 
     @api.model
     def _select_version(self):
@@ -35,16 +28,7 @@ class ImportBackend(models.Model):
         """
         return BACKEND_VERSIONS
 
-    @api.model
-    def _select_interval_type(self):
-        return [
-            ('hours', 'Hours'),
-            ('work_days', 'Work Days'),
-            ('days', 'Days'),
-            ('weeks', 'Weeks'),
-            ('months', 'Months')
-        ]
-
+    name = fields.Char(required=True)
     version = fields.Selection(
         selection='_select_version',
         string='Version',
@@ -53,23 +37,9 @@ class ImportBackend(models.Model):
     recordset_ids = fields.One2many(
         'import.recordset',
         'backend_id',
-        string='Record Sets',
+        string='Record Sets'
     )
     # cron stuff
-    cron_mode = fields.Boolean('Cron mode?')
-    cron_start_date = fields.Datetime(
-        'Start date',
-    )
-    cron_interval_number = fields.Integer('Interval number')
-    cron_interval_type = fields.Selection(
-        selection='_select_interval_type',
-        string='Interval type',
-    )
-    cron_id = fields.Many2one(
-        'ir.cron',
-        string='Related cron',
-        domain=lambda self: [('model', '=', self._name)],
-    )
     cron_master_recordset_id = fields.Many2one(
         'import.recordset',
         string='Master recordset',
@@ -98,64 +68,16 @@ class ImportBackend(models.Model):
         help=_("Tells you if a job is running for this backend."),
         readonly=True
     )
-    enable_user_mode = fields.Boolean(
-        'Enable user mode',
-        default=True,
-        help=_("Enabling user mode allows simple users "
-               "to use the quick wizard for importing recordsets on demand.")
-    )
-
-    @api.model
-    def get_cron_vals(self, backend=None):
-        backend = backend or self
-        return {
-            'name': 'Cron for import backend %s' % backend.name,
-            'model': backend._name,
-            'function': 'run_all',
-            'args': '(%s,)' % str(backend.id),
-            'interval_number': backend.cron_interval_number,
-            'interval_type': backend.cron_interval_type,
-            'nextcall': backend.cron_start_date,
-        }
-
-    def _update_or_create_cron(self):
-        """Update or create cron record if needed."""
-        if self.cron_mode:
-            cron_model = self.env['ir.cron']
-            cron_vals = self.get_cron_vals()
-            if not self.cron_id:
-                self.cron_id = cron_model.create(cron_vals)
-            else:
-                self.cron_id.write(cron_vals)
-
-    @api.model
-    def create(self, vals):
-        """ handle cron stuff
-        """
-        backend = super(ImportBackend, self).create(vals)
-        backend._update_or_create_cron()
-        return backend
-
-    @api.multi
-    def write(self, vals):
-        """ handle cron stuff
-        """
-        res = super(ImportBackend, self).write(vals)
-        for backend in self:
-            backend._update_or_create_cron()
-        return res
 
     @api.multi
     def unlink(self):
+        """Prevent delete if jobs are running."""
         for item in self:
             item.check_delete()
-        return super(ImportBackend, self).unlink()
+        return super(ImporterBackend, self).unlink()
 
-    @api.model
+    @api.multi
     def check_delete(self):
-        """ if debug mode is not ON check that we don't have
-        any jobs related to our sub records.
-        """
         if not self.debug_mode and self.job_running:
             raise exceptions.Warning(_('You must complete the job first!'))
 
@@ -174,20 +96,32 @@ class ImportBackend(models.Model):
             item.job_running = running
 
     @api.model
-    def run_all(self, backend_id=None):
-        """ run all recordset imports
-        """
-        backend = backend_id and self.browse(backend_id) or self
-        backend.ensure_one()
-        recordsets = backend.recordset_ids
-        if backend.cron_master_recordset_id:
+    def run_cron(self, backend_id):
+        # required by cron mixin
+        self.browse(backend_id).run_all()
+
+    @api.multi
+    def run_all(self):
+        """Run all recordset imports."""
+        self.ensure_one()
+        recordsets = self.recordset_ids
+        if self.cron_master_recordset_id:
             # clone and use it to run
-            recordsets = backend.cron_master_recordset_id.copy()
+            recordsets = self.cron_master_recordset_id.copy()
         for recordset in recordsets:
             recordset.run_import()
 
     @api.model
     def cron_cleanup_recordsets(self):
+        """Delete obsolete recordsets.
+
+        If you are running imports via cron and you create one recorset
+        per each run then you might end up w/ tons of old recordsets.
+
+        You can use `cron_cleanup_keep` to enable auto-cleanup.
+        Here we lookup for backends w/ this settings
+        and keep only latest recordsets.
+        """
         cleanup_logger.info('Looking for recorsets to cleanup.')
         backends = self.search([('cron_cleanup_keep', '>', 0)])
         to_clean = self.env['import.recordset']
@@ -210,8 +144,7 @@ class ImportBackend(models.Model):
 
     @api.multi
     def button_complete_jobs(self):
-        """ set all jobs to "completed" state.
-        """
+        """Set all jobs to "completed" state."""
         self.ensure_one()
         for recordset in self.recordset_ids:
             for record in recordset.record_ids:
@@ -219,8 +152,3 @@ class ImportBackend(models.Model):
                     record.job_id.button_done()
             if recordset.has_job() and not recordset.job_done():
                 recordset.job_id.button_done()
-
-    @api.onchange('enable_user_mode')
-    def _onchange_enable_user_mode(self):
-        """If user mode is enabled we want to run it with jobs by default."""
-        self.debug_mode = not self.enable_user_mode
