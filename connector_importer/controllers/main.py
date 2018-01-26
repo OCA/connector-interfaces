b0d73af43c46d39917e24ccb149a2441719acb75
Simone Orsi <simahawk@gmail.com>
2018-01-26 16:51:05 +0100

    [11.0] connector_importer

--- connector_importer/controllers/main.py
+++ connector_importer/controllers/main.py
@@ -1,17 +1,16 @@
-# -*- coding: utf-8 -*-
 # Author: Simone Orsi
-# Copyright 2017 Camptocamp SA
+# Copyright 2018 Camptocamp SA
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 from odoo import http
 from odoo.http import request
-# import werkzeug
 from ..utils.report_html import Reporter
 
 
 class ReportController(http.Controller):
-    """Controller for display import reports."""
+    """Controller to display import reports."""
 
+    # TODO: refactor this to use qweb template only
     @http.route(
         '/importer/import-recordset/<model("import.recordset"):recordset>',
         type='http', auth="user", website=False)
