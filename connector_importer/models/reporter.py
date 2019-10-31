eef45674dad0ec390f16da8a3ffb155effb3f30a
sebalix <seb@usr-src.org>
2019-10-31 16:56:43 +0100

    [IMP] connector_importer: black, isort

--- connector_importer/models/reporter.py
+++ connector_importer/models/reporter.py
@@ -3,11 +3,12 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 
-from odoo import models, api
+import base64
 import csv
 import io
 import time
-import base64
+
+from odoo import api, models
 
 
 class ReporterMixin(models.AbstractModel):
@@ -19,9 +20,10 @@ class ReporterMixin(models.AbstractModel):
     The summary can be anything you like: you are in total control of it.
     See the CSV example for a real case.
     """
-    _name = 'reporter.mixin'
 
-    report_extension = '.txt'
+    _name = "reporter.mixin"
+
+    report_extension = ".txt"
 
     @api.model
     def report_get(self, recordset, **options):
@@ -43,11 +45,7 @@ class ReporterMixin(models.AbstractModel):
         """Retrieve report file's metadata."""
         fname = str(time.time())
         ext = self.report_extension
-        return {
-            'filename': fname,
-            'ext': ext,
-            'complete_filename': fname + ext,
-        }
+        return {"filename": fname, "ext": ext, "complete_filename": fname + ext}
 
 
 class CSVReporter(models.AbstractModel):
@@ -76,31 +74,34 @@ class CSVReporter(models.AbstractModel):
 
     In this way the end user can check side by side which lines went wrong.
     """
-    _name = 'reporter.csv'
-    _inherit = 'reporter.mixin'
 
-    report_extension = '.csv'
+    _name = "reporter.csv"
+    _inherit = "reporter.mixin"
+
+    report_extension = ".csv"
     # columns to track/add
-    report_keys = ['skipped', 'errored']
+    report_keys = ["skipped", "errored"]
     # Flag to determine if status report must be grouped by status.
     # If `True` report result will be merged by status (errored, skipped, ...)
     report_group_by_status = True
 
-    def report_get_writer(self, fileout, columns,
-                          delimiter=';', quotechar='"'):
+    def report_get_writer(self, fileout, columns, delimiter=";", quotechar='"'):
         writer = csv.DictWriter(
-            fileout, columns,
+            fileout,
+            columns,
             delimiter=delimiter,
             quoting=csv.QUOTE_NONNUMERIC,
-            quotechar=quotechar)
+            quotechar=quotechar,
+        )
         writer.writeheader()
         return writer
 
     def report_add_line(self, writer, item):
         writer.writerow(item)
 
-    def report_get_columns(self, recordset, orig_content,
-                           extra_keys=None, delimiter=';', quotechar='"'):
+    def report_get_columns(
+        self, recordset, orig_content, extra_keys=None, delimiter=";", quotechar='"'
+    ):
         """Retrieve columns by recordset.
 
         :param recordset: instance of recordset.
@@ -111,7 +112,8 @@ class CSVReporter(models.AbstractModel):
         # read only the 1st line of the original file
         if orig_content:
             reader = csv.reader(
-                orig_content[:1], delimiter=delimiter, quotechar=quotechar)
+                orig_content[:1], delimiter=delimiter, quotechar=quotechar
+            )
             columns = next(reader)
             return columns + extra_keys
         return extra_keys
@@ -119,16 +121,12 @@ class CSVReporter(models.AbstractModel):
     def report_do(self, recordset, fileout, **options):
         """Produce report."""
         json_report = recordset.get_report()
-        report_keys = options.get('report_keys', self.report_keys)
-        group_by_status = options.get(
-            'group_by_status', self.report_group_by_status)
+        report_keys = options.get("report_keys", self.report_keys)
+        group_by_status = options.get("group_by_status", self.report_group_by_status)
 
-        model_keys = [
-            x for x in json_report.keys() if not x.startswith('_')]
+        model_keys = [x for x in json_report.keys() if not x.startswith("_")]
 
-        extra_keys = [
-            self._report_make_key(x) for x in report_keys
-        ]
+        extra_keys = [self._report_make_key(x) for x in report_keys]
         if not group_by_status:
             # we produce one column per-model per-status
             for model in model_keys:
@@ -141,14 +139,18 @@ class CSVReporter(models.AbstractModel):
         quotechar = source.csv_quotechar
 
         columns = self.report_get_columns(
-            recordset, orig_content,
-            extra_keys=extra_keys, delimiter=delimiter, quotechar=quotechar)
+            recordset,
+            orig_content,
+            extra_keys=extra_keys,
+            delimiter=delimiter,
+            quotechar=quotechar,
+        )
 
         writer = self.report_get_writer(
-            fileout, columns, delimiter=delimiter, quotechar=quotechar)
+            fileout, columns, delimiter=delimiter, quotechar=quotechar
+        )
 
-        reader = csv.DictReader(
-            orig_content, delimiter=delimiter, quotechar=quotechar)
+        reader = csv.DictReader(orig_content, delimiter=delimiter, quotechar=quotechar)
 
         self._report_do(
             json_report=json_report,
@@ -156,33 +158,33 @@ class CSVReporter(models.AbstractModel):
             writer=writer,
             model_keys=model_keys,
             report_keys=report_keys,
-            group_by_status=group_by_status
+            group_by_status=group_by_status,
         )
 
     def _report_do(
-            self,
-            json_report=None,
-            reader=None,
-            writer=None,
-            model_keys=None,
-            report_keys=None,
-            group_by_status=True):
+        self,
+        json_report=None,
+        reader=None,
+        writer=None,
+        model_keys=None,
+        report_keys=None,
+        group_by_status=True,
+    ):
 
         line_handler = self._report_line_by_model_and_status
         if group_by_status:
             line_handler = self._report_line_by_status
 
-        grouped = self._report_group_by_line(
-            json_report, model_keys, report_keys)
+        grouped = self._report_group_by_line(json_report, model_keys, report_keys)
 
         for line in reader:
             line_handler(line, reader.line_num, grouped, model_keys)
             self.report_add_line(writer, line)
 
-    def _report_make_key(self, key, model=''):
+    def _report_make_key(self, key, model=""):
         if model:
-            return '[R] {}: {}'.format(model, key)
-        return '[R] {}'.format(key)
+            return "[R] {}: {}".format(model, key)
+        return "[R] {}".format(key)
 
     def _report_group_by_line(self, json_report, model_keys, report_keys):
         """Group report items by line number.
@@ -229,27 +231,22 @@ class CSVReporter(models.AbstractModel):
             for key in report_keys:
                 by_line.setdefault(key, {})
                 for item in by_model.get(key, []):
-                    by_line[key].setdefault(
-                        item['line_nr'], []
-                    ).append(item)
+                    by_line[key].setdefault(item["line_nr"], []).append(item)
         return by_line
 
-    def _report_line_by_model_and_status(
-            self, line, line_num, grouped, model_keys):
+    def _report_line_by_model_and_status(self, line, line_num, grouped, model_keys):
         """Get one column per each pair model-status."""
         for model in model_keys:
             for status, lines in grouped.items():
                 # get info on current line if any
                 line_info = lines.get(line_num, {})
                 # add the extra report column anyway
-                line[self._report_make_key(model, status)] = \
-                    line_info.get('message')
+                line[self._report_make_key(model, status)] = line_info.get("message")
 
-    def _report_line_by_status(
-            self, line, line_num, grouped, model_keys):
+    def _report_line_by_status(self, line, line_num, grouped, model_keys):
         """Get one column per each status containing all modelss messages."""
         for status, by_line in grouped.items():
             line_info = by_line.get(line_num, [])
-            line[self._report_make_key(status)] = '\n'.join([
-                '{model}: {message}'.format(**item) for item in line_info
-            ])
+            line[self._report_make_key(status)] = "\n".join(
+                ["{model}: {message}".format(**item) for item in line_info]
+            )
