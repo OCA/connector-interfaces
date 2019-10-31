9532f954775b4fc094ab0f6e232528beceb862e2
sebalix <seb@usr-src.org>
2019-10-31 16:56:43 +0100

    [IMP] connector_importer: black, isort

--- connector_importer/controllers/main.py
+++ connector_importer/controllers/main.py
@@ -4,6 +4,7 @@
 
 from odoo import http
 from odoo.http import request
+
 from ..utils.report_html import Reporter
 
 
@@ -13,12 +14,11 @@ class ReportController(http.Controller):
     # TODO: refactor this to use qweb template only
     @http.route(
         '/importer/import-recordset/<model("import.recordset"):recordset>',
-        type='http', auth="user", website=False)
+        type="http",
+        auth="user",
+        website=False,
+    )
     def full_report(self, recordset, **kwargs):
         reporter = Reporter(recordset.jsondata, detailed=1)
-        values = {
-            'recordset': recordset,
-            'report': reporter.html(wrapped=0),
-
-        }
+        values = {"recordset": recordset, "report": reporter.html(wrapped=0)}
         return request.render("connector_importer.recordset_report", values)
--- connector_importer/models/cron_mixin.py
+++ connector_importer/models/cron_mixin.py
@@ -2,7 +2,7 @@
 # Copyright 2018 Camptocamp SA
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from odoo import models, fields, api
+from odoo import api, fields, models
 
 
 class CronMixin(models.AbstractModel):
@@ -16,51 +16,51 @@ class CronMixin(models.AbstractModel):
 
     You have to implement the method `run_cron`.
     """
-    _name = 'cron.mixin'
-    _description = 'Cron Mixin'
 
-    cron_mode = fields.Boolean('Cron mode?')
-    cron_start_date = fields.Datetime('Start date')
-    cron_interval_number = fields.Integer('Interval number')
+    _name = "cron.mixin"
+    _description = "Cron Mixin"
+
+    cron_mode = fields.Boolean("Cron mode?")
+    cron_start_date = fields.Datetime("Start date")
+    cron_interval_number = fields.Integer("Interval number")
     cron_interval_type = fields.Selection(
-        selection='_select_interval_type',
-        string='Interval type',
+        selection="_select_interval_type", string="Interval type"
     )
     cron_id = fields.Many2one(
-        'ir.cron',
-        string='Related cron',
+        "ir.cron",
+        string="Related cron",
         domain=lambda self: [
-            ('model_id', '=', self.env['ir.model']._get_id(self._name))
+            ("model_id", "=", self.env["ir.model"]._get_id(self._name))
         ],
     )
 
     @api.model
     def _select_interval_type(self):
         return [
-            ('hours', 'Hours'),
-            ('work_days', 'Work Days'),
-            ('days', 'Days'),
-            ('weeks', 'Weeks'),
-            ('months', 'Months')
+            ("hours", "Hours"),
+            ("work_days", "Work Days"),
+            ("days", "Days"),
+            ("weeks", "Weeks"),
+            ("months", "Months"),
         ]
 
     @api.model
     def get_cron_vals(self):
-        model_id = self.env['ir.model']._get_id(self._name)
+        model_id = self.env["ir.model"]._get_id(self._name)
         return {
-            'name': 'Cron for import backend %s' % self.name,
-            'model_id': model_id,
-            'state': 'code',
-            'code': 'model.run_cron(%d)' % self.id,
-            'interval_number': self.cron_interval_number,
-            'interval_type': self.cron_interval_type,
-            'nextcall': self.cron_start_date,
+            "name": "Cron for import backend %s" % self.name,
+            "model_id": model_id,
+            "state": "code",
+            "code": "model.run_cron(%d)" % self.id,
+            "interval_number": self.cron_interval_number,
+            "interval_type": self.cron_interval_type,
+            "nextcall": self.cron_start_date,
         }
 
     def _update_or_create_cron(self):
         """Update or create cron record if needed."""
         if self.cron_mode:
-            cron_model = self.env['ir.cron']
+            cron_model = self.env["ir.cron"]
             cron_vals = self.get_cron_vals()
             if not self.cron_id:
                 self.cron_id = cron_model.create(cron_vals)
