7ae05149a6c8ec9ab8a0a5228588d30ed063487c
sebalix <seb@usr-src.org>
2019-11-05 09:17:03 +0100

    [IMP] connector_importer_demo: black, isort

--- connector_importer_demo/hooks.py
+++ connector_importer_demo/hooks.py
@@ -3,7 +3,7 @@
 
 import base64
 
-from odoo import api, SUPERUSER_ID
+from odoo import SUPERUSER_ID, api
 from odoo.modules.module import get_module_resource
 
 
@@ -11,10 +11,10 @@ def post_init_hook(cr, _):
     with api.Environment.manage():
         env = api.Environment(cr, SUPERUSER_ID, {})
         csv_path = get_module_resource(
-            'connector_importer_demo', 'example', 'res.partner.csv')
-        with open(csv_path, 'rb') as file_:
+            "connector_importer_demo", "example", "res.partner.csv"
+        )
+        with open(csv_path, "rb") as file_:
             data = file_.read()
-            source_csv = env.ref(
-                'connector_importer_demo.import_source_csv_partner')
+            source_csv = env.ref("connector_importer_demo.import_source_csv_partner")
             data_b64 = base64.b64encode(data)
             source_csv.csv_file = data_b64
