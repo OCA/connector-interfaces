1721ea592adcda657c90faf543f5e0d984ad1453
sebalix <seb@usr-src.org>
2019-11-07 14:28:35 +0100

    [IMP] connector_importer: handle the unique key as an external/XML ID
    'importer.record' and 'importer.odoorecord.handler' components have been
    updated to handle the 'odoo_unique_key' attribute as an XML-ID if the
    new attribute 'odoo_unique_key_is_xmlid' is set to 'True'.
    This XML-ID will then be used as usual to find an existing record, and
    will be set on the created record if it doesn't already exist.

--- connector_importer/components/importer.py
+++ connector_importer/components/importer.py
@@ -66,7 +66,9 @@ class RecordImporter(Component):
     _record_handler_usage = 'odoorecord.handler'
     _tracking_handler_usage = 'tracking.handler'
     # a unique key (field name) to retrieve the odoo record
+    # if this key is an external/XML ID, set 'odoo_unique_key_is_xmlid' to True
     odoo_unique_key = ''
+    odoo_unique_key_is_xmlid = False
 
     def _init_importer(self, recordset):
         self.recordset = recordset
@@ -74,7 +76,8 @@ class RecordImporter(Component):
         self.record_handler = self.component(usage=self._record_handler_usage)
         self.record_handler._init_handler(
             importer=self,
-            unique_key=self.odoo_unique_key
+            unique_key=self.odoo_unique_key,
+            unique_key_is_xmlid=self.odoo_unique_key_is_xmlid
         )
         # tracking handler is responsible for logging and chunk reports
         self.tracker = self.component(usage=self._tracking_handler_usage)
@@ -168,7 +171,8 @@ class RecordImporter(Component):
             }
         missing = (not dest_key.startswith('__') and
                    values.get(dest_key) is None)
-        if missing:
+        is_xmlid = (dest_key == unique_key and self.odoo_unique_key_is_xmlid)
+        if missing and not is_xmlid:
             msg = 'MISSING REQUIRED DESTINATION KEY={}'.format(dest_key)
             if unique_key and values.get(unique_key):
                 msg += ': {}={}'.format(
