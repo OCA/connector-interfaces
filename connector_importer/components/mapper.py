43c302c726ed3c1d3b03d11260f2375885281e42
Damien Crier <damien.crier@camptocamp.com>
2021-12-15 14:55:37 +0100

    [FIX] management of the _xml_id::module.name notation

--- connector_importer/components/mapper.py
+++ connector_importer/components/mapper.py
@@ -2,6 +2,8 @@
 # Copyright 2018 Camptocamp SA
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
+from odoo import _, exceptions
+
 from odoo.addons.component.core import Component
 from odoo.addons.connector.components.mapper import mapping
 
@@ -69,7 +71,7 @@ class ImportMapper(Component):
         # whereas `$record_xmlid` is the xmlid to retrieve
         # and ``$record_field_value` is the field to be used as value.
         # Example:
-        # ('company_id', '_xmlid:base.main_company:id'),
+        # ('company_id', '_xmlid::base.main_company:id'),
     ]
 
     @mapping
@@ -81,7 +83,11 @@ class ImportMapper(Component):
         values = {}
         for k, v in self.defaults:
             if isinstance(v, str) and v.startswith("_xmlid::"):
-                xmlid, field_value = v.split("::")[1:]
+                xml_id_value_list = v.split("::")[1:]
+                if not xml_id_value_list:
+                    raise exceptions.UserError(_("Malformated xml id ref"))
+                xml_id_value = xml_id_value_list[0]
+                xmlid, field_value = xml_id_value.split(":")
                 v = self.env.ref(xmlid)[field_value]
             values[k] = v
         return values
