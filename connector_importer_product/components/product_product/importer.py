92b4544ace1d033f3d92e69926e788bf6dc1404d
Simone Orsi <simahawk@gmail.com>
2022-04-06 16:49:19 +0200

    c_importer_product: make importer and mapper smarter
    The mapper now uses the dynanic mapper to automatically
    find and convert values from source to destination.
    
    We can trash many direct mappings!
    
    Additionally, the supplier is now imported automatically.

--- connector_importer_product/components/product_product/importer.py
+++ connector_importer_product/components/product_product/importer.py
@@ -3,21 +3,11 @@
 
 from odoo.addons.component.core import Component
 
-from ...utils import sanitize_external_id
-
 
 class ProductProductRecordImporter(Component):
     _name = "product.product.importer"
     _inherit = ["common.product.importer"]
     _apply_on = "product.product"
-    odoo_unique_key = "id"
-    odoo_unique_key_is_xmlid = True
-
-    def prepare_line(self, line):
-        res = super().prepare_line(line)
-        res["id"] = sanitize_external_id(line["id"])
-        res["template_default_code"] = sanitize_external_id(
-            line["template_default_code"]
-        )
-        res["categ_id"] = sanitize_external_id(line["categ_id"])
-        return res
+    _mapper_name = "product.product.mapper"
+    odoo_unique_key = "default_code"
+    # _break_on_error = True
--- connector_importer_product/components/product_product/mapper.py
+++ connector_importer_product/components/product_product/mapper.py
@@ -2,42 +2,76 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
 from odoo.addons.component.core import Component
-from odoo.addons.connector.components.mapper import mapping
-from odoo.addons.connector_importer.utils.mapper_utils import xmlid_to_rel
+from odoo.addons.connector_importer.utils.mapper_utils import backend_to_rel
+
+
+def _get_extra_fields(rel_model, record, prefix):
+    extra_fields = []
+    for k in record.keys():
+        if not k.startswith(prefix):
+            continue
+        real_fname = k[len(prefix) :]
+        if real_fname in rel_model._fields:
+            extra_fields.append(real_fname)
+    return extra_fields
+
+
+def _get_or_create_supplier_partner_id(env, name):
+    model = env["res.partner"]
+    partner = model.search([("name", "=", name)], limit=1)
+    if not partner:
+        partner = partner.create({"name": name})
+    return partner.id
+
+
+def missing_supplier_handler(self, rel_model, record):
+    """Handle create of missing supplier.
+
+    `supplier` key/col will be used for the name.
+    Any additional product.supplierinfo field can be passed
+    by using its real name prefixed by `supplier_`.
+    Eg: `supplier_product_name`, `supplier_product_code`, etc.
+    """
+
+    values = {
+        "name": _get_or_create_supplier_partner_id(rel_model.env, record["supplier"]),
+    }
+    # TODO: we could generalize this approach and always look for all keys
+    # startign with key + "." then we can apply the same conversion
+    # as for `dynamic_fields` by field type on the related record.
+    prefix = "supplier."
+    extra_fields = _get_extra_fields(rel_model, record, prefix)
+    for k in extra_fields:
+        if record.get(k) is not None:
+            values[k] = record[k]
+    # TODO: we should rely on the supplier info importer for this job
+    # but we should be able to configure the prefix in the options.
+    return rel_model.create(values)
 
 
 class ProductProductMapper(Component):
     _name = "product.product.mapper"
-    _inherit = "importer.base.mapper"
+    # Non mapped fields will be managed by the dynamic mapper
+    _inherit = "importer.mapper.dynamic"
     _apply_on = "product.product"
+    _mapper_usage = "importer.mapper"
 
     direct = [
-        # "id" needs to be in the mapped values to be converted as XML-ID
-        # TODO: need to allow the use of fake destination fields like '_xmlid'
-        # in direct mapping here:
-        # https://github.com/OCA/connector/blob/13.0/connector/components/mapper.py#L891
-        ("id", "id"),
-        ("name", "name"),
-        ("default_code", "default_code"),
-        ("barcode", "barcode"),
-        ("list_price", "list_price"),
-        ("standard_price", "standard_price"),
-        ("type", "type"),
-        (xmlid_to_rel("uom_id"), "uom_id"),
-        (xmlid_to_rel("categ_id"), "categ_id"),
+        (
+            backend_to_rel(
+                "supplier",
+                search_field="name.name",
+                create_missing=True,
+                create_missing_handler=missing_supplier_handler,
+            ),
+            "seller_ids",
+        ),
     ]
-    required = {"categ_id": "categ_id"}
+    required = {
+        "default_code": "default_code",
+        "name": "name",
+    }
     translatable = ["name"]
 
-    @mapping
-    def product_tmpl_id(self, record):
-        if record.get("template_default_code"):
-            template = self.env.ref(
-                record["template_default_code"], raise_if_not_found=False
-            )
-            # If no product.template is found, it'll be created automatically
-            # as usual when the product.product is created.Then the importer
-            # will set its External ID.
-            if template:
-                return {"product_tmpl_id": template.id}
-        return {}
+    # def finalize(self, map_record, values):
+    #     return values
