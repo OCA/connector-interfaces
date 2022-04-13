d44dbd66a9072f052b4b233c5f9135cc0fcce60e
Simone Orsi <simahawk@gmail.com>
2022-04-13 12:33:16 +0200

    c_importer_product: improve product import

--- connector_importer_product/components/product_product/mapper.py
+++ connector_importer_product/components/product_product/mapper.py
@@ -2,51 +2,8 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
 from odoo.addons.component.core import Component
+from odoo.addons.connector.components.mapper import mapping
 from odoo.addons.connector_importer.utils.mapper_utils import backend_to_rel
-
-
-def _get_extra_fields(rel_model, record, prefix):
-    extra_fields = []
-    for k in record.keys():
-        if not k.startswith(prefix):
-            continue
-        real_fname = k[len(prefix) :]
-        if real_fname in rel_model._fields:
-            extra_fields.append(real_fname)
-    return extra_fields
-
-
-def _get_or_create_supplier_partner_id(env, name):
-    model = env["res.partner"]
-    partner = model.search([("name", "=", name)], limit=1)
-    if not partner:
-        partner = partner.create({"name": name})
-    return partner.id
-
-
-def missing_supplier_handler(self, rel_model, record):
-    """Handle create of missing supplier.
-
-    `supplier` key/col will be used for the name.
-    Any additional product.supplierinfo field can be passed
-    by using its real name prefixed by `supplier_`.
-    Eg: `supplier_product_name`, `supplier_product_code`, etc.
-    """
-
-    values = {
-        "name": _get_or_create_supplier_partner_id(rel_model.env, record["supplier"]),
-    }
-    # TODO: we could generalize this approach and always look for all keys
-    # startign with key + "." then we can apply the same conversion
-    # as for `dynamic_fields` by field type on the related record.
-    prefix = "supplier."
-    extra_fields = _get_extra_fields(rel_model, record, prefix)
-    for k in extra_fields:
-        if record.get(k) is not None:
-            values[k] = record[k]
-    # TODO: we should rely on the supplier info importer for this job
-    # but we should be able to configure the prefix in the options.
-    return rel_model.create(values)
 
 
 class ProductProductMapper(Component):
@@ -59,19 +16,41 @@ class ProductProductMapper(Component):
     direct = [
         (
             backend_to_rel(
-                "supplier",
-                search_field="name.name",
-                create_missing=True,
-                create_missing_handler=missing_supplier_handler,
+                "tmpl_default_code",
+                # When you want to related a template via code
+                # this kind of search is mandatory
+                # because Odoo will set the template.default_code to false
+                # every time it has more than one variant.
+                # We assume that if you are using `tmpl_default_code`
+                # all the variants in your dataset either have the same code
+                # or at least one of them has default_code = tmpl_default_code.
+                search_field="product_variant_ids.default_code",
             ),
-            "seller_ids",
+            "product_tmpl_id",
         ),
     ]
     required = {
-        "default_code": "default_code",
         "name": "name",
     }
     translatable = ["name"]
 
-    # def finalize(self, map_record, values):
-    #     return values
+    defaults = [("sale_ok", True)]
+
+    @mapping
+    def code(self, record):
+        """Ensure there's always a default code.
+
+        We must have a default code to univocally find the template
+        if it's already created.
+        In your dataset you might not care about having a default code per variant
+        which means that likely you'll have only `tmpl_default_code`.
+        If that's the case, we case, the 1st variant that will create the product
+        and its template, won't have a default_code for it and any subsequent lookup
+        of the template will fail.
+        """
+        vals = {}
+        for key in ("default_code", "tmpl_default_code"):
+            if record.get(key):
+                vals["default_code"] = record[key]
+                break
+        return vals
