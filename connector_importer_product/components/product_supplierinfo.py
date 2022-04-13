70f22f8e4f831bad01b900c7c2fd32daacb7fd80
Simone Orsi <simahawk@gmail.com>
2022-04-13 12:33:51 +0200

    c_importer_product: improve supplierinfo import

--- connector_importer_product/components/product_supplierinfo.py
+++ connector_importer_product/components/product_supplierinfo.py
@@ -2,19 +2,47 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
 from odoo.addons.component.core import Component
+from odoo.addons.connector_importer.utils.mapper_utils import backend_to_rel
 
-from ..utils import sanitize_external_id
 
-
-class ProductSupplierinfoImporter(Component):
-    _name = "product.supplierinfo.importer"
-    _inherit = ["common.product.importer", "importer.record.csv.std"]
+class ProductSupplierinfoMapper(Component):
+    _name = "product.supplierinfo.mapper"
+    # Non mapped fields will be managed by the dynamic mapper
+    _inherit = "importer.mapper.dynamic"
     _apply_on = "product.supplierinfo"
-    _use_xmlid = False
+    _usage = "importer.mapper"
 
-    def prepare_line(self, line):
-        res = super().prepare_line(line)
-        for key in line.keys():
-            if key == "id" or key.endswith("/id"):
-                res[key] = sanitize_external_id(line[key])
-        return res
+    direct = [
+        (
+            backend_to_rel(
+                "tmpl_default_code",
+                # See motiviation in product_product.mapper.
+                search_field="product_variant_ids.default_code",
+            ),
+            "product_tmpl_id",
+        ),
+    ]
+    required = {
+        "__name": "name",
+        "__tmpl": "product_tmpl_id",
+    }
+
+    # TODO: set partner supplier rank
+
+
+class ProductSupplierinfoRecordHandler(Component):
+    """Interact w/ odoo importable records."""
+
+    _name = "product.supplierinfo.handler"
+    _inherit = "importer.odoorecord.handler"
+    _apply_on = "product.supplierinfo"
+
+    def odoo_find_domain(self, values, orig_values):
+        domain = [
+            (self.unique_key, "=", values[self.unique_key]),
+            ("product_tmpl_id", "=", values["product_tmpl_id"]),
+        ]
+        for key in ("product_id",):
+            if values.get("product_id"):
+                domain.append((key, "=", values[key]))
+        return domain
