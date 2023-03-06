08141601f95a52ad5b596c7cef81ab02ec0d6754
Simone Orsi <simahawk@gmail.com>
2023-03-06 14:05:35 +0100

    connector_importer_source_sftp: fix pin cryptography<37

--- connector_importer_source_sftp/__manifest__.py
+++ connector_importer_source_sftp/__manifest__.py
@@ -5,7 +5,7 @@
 {
     "name": "Connector Importer Source SFTP",
     "summary": """Add import source capable of loading files from SFTP.""",
-    "version": "15.0.1.0.0",
+    "version": "15.0.1.0.1",
     "license": "AGPL-3",
     "author": "Camptocamp,ACSONE,Odoo Community Association (OCA)",
     "maintainers": ["simahawk", "sebalix"],
@@ -17,6 +17,16 @@
         "storage_backend_sftp",
     ],
     # fmt: on
+    # Place an upper bound on cryptography version to be compatible with
+    # pyopenssl 19 mentioned in Odoo 15's requirements.txt. If we don't do
+    # this, installing this module will try to upgrade cryptography to the latest
+    # version because the minimum required version in pyopenssl (>=3.1) is greater than
+    # version 2.6 (from Odoo's requirement.txt). Since cryptography/pyopenssl don't
+    # declare minimum supported versions, this lead to inconsistencies.
+    # https://github.com/OCA/server-auth/issues/424
+    # https://github.com/OCA/storage/pull/247
+    # https://github.com/OCA/connector-interfaces/pull/94
+    "external_dependencies": {"python": ["cryptography<37"]},
     "data": ["views/source_csv_sftp.xml", "security/ir.model.access.csv"],
     "demo": ["demo/storage_backend_demo.xml", "demo/import_source_demo.xml"],
 }
