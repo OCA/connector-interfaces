d24f14b44363b4ddf9bce95c731fef58c5263382
Simone Orsi <simahawk@gmail.com>
2023-07-26 11:58:49 +0200

    c_importer_product: clean test_record_handler dead code

--- connector_importer_product/tests/test_record_handler.py
+++ connector_importer_product/tests/test_record_handler.py
@@ -1,10 +1,6 @@
 # Copyright 2022 Camptocamp SA
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
-try:
-    pass
-except ImportError:
-    pass
 from odoo.tools import DotDict
 
 from .common import TestImportProductBase
