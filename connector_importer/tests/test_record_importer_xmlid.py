4ea4e1211c12249ac03596bff8b02393fc2b69de
Ricardoalso <ricardo.almeidasoares@camptocamp.com>
2024-10-04 15:04:23 +0200

    Jobs are not spawn by default if not in debug mode

--- connector_importer/tests/test_record_importer_xmlid.py
+++ connector_importer/tests/test_record_importer_xmlid.py
@@ -51,8 +51,8 @@ class TestRecordImporterXMLID(TestImporterBase):
         report = self.recordset.get_report()
         model = "res.partner"
         expected = {model: {"created": 10, "errored": 0, "updated": 0, "skipped": 0}}
-        delayable = res[model]
-        self.assertEqual(delayable.result, expected[model])
+        result = res[model]
+        self.assertEqual(result, expected[model])
         for k, v in expected[model].items():
             self.assertEqual(len(report[model][k]), v)
         self.assertEqual(self.env[model].search_count([("ref", "like", "id_%")]), 10)
