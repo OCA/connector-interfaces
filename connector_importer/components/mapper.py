3e9b0c0ad59dba576eb147b6f8be55770f73e637
Ricardoalso <ricardo.almeidasoares@camptocamp.com>
2025-08-07 14:07:38 +0200

    connector_importer: refactor required keys handling

--- connector_importer/components/mapper.py
+++ connector_importer/components/mapper.py
@@ -53,10 +53,11 @@ class ImportMapper(Component):
         The recordset will use this to show required fields to users.
         """
         req = dict(self.required)
-        req.update(self.work.options.mapper.get("required_keys", {}))
 
         if self.required_keys_ignore_mapper:
             req = self.work.options.mapper.get("required_keys", {})
+        else:
+            req.update(self.work.options.mapper.get("required_keys", {}))
 
         return req
 
