b469fc6dae89845052f02272b44e9d261229eb59
Iván Todorovich <ivan.todorovich@gmail.com>
2025-12-16 15:46:21 -0300

    [FIX] connector_importer: wrong field name used in tests - title/title_id
    The new warning caught this silent error, so this fixes it.
    
    ```
    WARNING odoo [importer]: Field `title_id` not found in model `res.partner`
    ```

--- connector_importer/tests/test_mapper.py
+++ connector_importer/tests/test_mapper.py
@@ -119,13 +119,14 @@ class TestRecordsetImporter(TestImporterBase):
             "xid::category_id": """
                 base.res_partner_category_0,base.res_partner_category_2
             """,
-            "title_id": "Doctor",
+            "title": "Doctor",
         }
         expected = {
             "name": "John Doe",
             "ref": "12345",
             "parent_id": self.env.ref("base.res_partner_10").id,
             "category_id": [(6, 0, categs.ids)],
+            "title": self.env.ref("base.res_partner_title_doctor").id,
         }
         self.assertEqual(mapper.dynamic_fields(rec), expected)
 
