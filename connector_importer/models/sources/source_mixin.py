833427d080edcc32b5bab9c52016b6e72edde84d
Akim Juillerat <akim.juillerat@camptocamp.com>
2026-03-02 22:08:29 +0100

    fixup! [FIX][18.0] connector_importer: allow sudo for the user to access ir.ui.view model

--- connector_importer/models/sources/source_mixin.py
+++ connector_importer/models/sources/source_mixin.py
@@ -113,14 +113,11 @@ class ImportSource(models.AbstractModel):
 
     def get_config_view_id(self):
         """Retrieve configuration view."""
-        # Allow sudo to access ``ir.ui.view`` while
-        # the user with ``group_connector_manager`` may not have access to it.
-        return (
-            self.env["ir.ui.view"]
-            .sudo()
-            .search([("model", "=", self._name), ("type", "=", "form")], limit=1)
-            .id
-        )
+        # DEPRECATED, use get_formview_id instead
+        return self.get_formview_id()
+    
+    def get_formview_id(self, access_uid=None):
+        return self.env["ir.ui.view"].default_view(self._name, "form") or False
 
     def get_reporter(self):
         """Retrieve a specific reporter for this source.
